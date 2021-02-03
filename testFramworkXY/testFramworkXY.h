ec5cf70887d9338744ef83b3197abcdd8e05e4f1
Mohamed Hashem <mohamedhashem@xtrava.co>
2021-02-03 09:29:40 +0200

    Initial Commit

---
+++ testFramworkXY/testFramworkXY.h
@@ -0,0 +1,18 @@
+//
+//  testFramworkXY.h
+//  testFramworkXY
+//
+//  Created by mohamed hashem on 03/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFramworkXY.
+FOUNDATION_EXPORT double testFramworkXYVersionNumber;
+
+//! Project version string for testFramworkXY.
+FOUNDATION_EXPORT const unsigned char testFramworkXYVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramworkXY/PublicHeader.h>
+
+
